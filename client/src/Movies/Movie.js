783ec11fff8016258fa3f2fbd52e296f4487d00c
Jessica <Jessica@users-iMac.local>
2019-12-09 21:54:53 -0500

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    // const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    const id = props.match.params.id;
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
@@ -18,22 +20,22 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -48,8 +50,9 @@ const Movie = (props) => {
             {star}
           </div>
         ))}
-      </div>
-      <div className="save-button">Save</div>
+      </div> */}
+      <MovieCard movie = {movie} />
+      <div className="save-button" onClick = {saveMovie}>Save</div>
     </div>
   );
 }
